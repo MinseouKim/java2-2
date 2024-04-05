6ecd45641312f25ba474c51749a67180941f1436
MinseouKim <202130103@email.daelim.ac.kr>
2024-04-05 15:34:56 +0900

    소스파일로 이동

--- src/practice3.java
+++ src/practice3.java
@@ -11,7 +11,7 @@ public class practice3 {
         dividend =scanner.nextInt();
         System.out.print("피제수 입력하시오 :");
         try{
-            System.out.println(dividend"를"+ divisor + "로 나누면 몫은" + dividend/divisor + "입니다.");
+            System.out.println(dividend + "를"+ divisor + "로 나누면 몫은" + dividend/divisor + "입니다.");
         }
         catch(ArithmeticException e){ //ArithmeticException 예외 처리 코드
             System.out.println("a으로 나눌수 없습니다.");
